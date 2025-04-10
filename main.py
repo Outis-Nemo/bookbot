0034b3bd5d26601ac05d490caa437a26c7f2bcb7
Outis-Nemo <socraticsandcastle@gmail.com>
2025-04-10 10:36:44 -0500

    C: Wrote a program that counts the number of words in a text and a character count

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from stats import get_num_words
 from stats import get_num_char
+import sys
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
-    print(get_num_char(book_text))
-
-
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    char_dictionary = get_num_char(book_text)
+    ordered_keys = sorted(char_dictionary, reverse = False)
+    for key in ordered_keys:
+        print(f"{key}: {char_dictionary[key]}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ def get_num_char(text):
     char_dictionary = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 
                        'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0,
                        'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0,
-                       'y' : 0, 'z' : 0}
+                       'y' : 0, 'z' : 0, 'æ' : 0, 'â' : 0, 'ê' : 0, 'ë' : 0, 'ô' : 0}
     for char in text:
         temp = char.lower()
         if temp in char_dictionary:
